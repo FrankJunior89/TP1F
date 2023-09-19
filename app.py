7e6471e7006a56bbede22c36cc63c80654ab97fc
fngnigue <ngniguefrankjunior@gmail.com>
2023-09-19 15:57:22 +0200

    t commit

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask
 
+def hello_world():
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=UA-285827553-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-285827553-1');
+    </script>
+    """
+    return prefix_google + "Hello World"
+
 app = Flask(__name__)
 
 @app.route('/')
