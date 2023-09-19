d1121b7968fba0d01d4647ac6054ced756e3e755
fngnigue <ngniguefrankjunior@gmail.com>
2023-09-19 16:12:03 +0200

    third commit

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 from flask import Flask
 
-def hello_world():
-    prefix_google = """
-    <!-- Google tag (gtag.js) -->
-    <script async
-    src="https://www.googletagmanager.com/gtag/js?id=UA-285827553-1"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-285827553-1');
-    </script>
-    """
-    return prefix_google + "Hello World"
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello world !"
+def hello_world():
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=G-55763RC5D7"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-G-55763RC5D7');
+    </script>
+    """
+    return prefix_google + "Hello World"
 
 if __name__ == "__main__":
     app.run()
